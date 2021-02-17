02f98bd0bce31e9c1bcc9a9f999793f91628f8f9
EthanRDoesMC <ethan.r.chaffin@gmail.com>
2021-02-17 17:54:35 +0000

    Update setuptheos.sh

--- setuptheos.sh
+++ setuptheos.sh
@@ -79,11 +79,12 @@ unzip master.zip -d $TMP
 mv $TMP/sdks-master/*.sdk $THEOS/sdks
 rm -r master.zip $TMP
 fi
-curl -LO https://github.com/EthanRDoesMC/sdks/archive/master.zip
+curl -LO https://github.com/EthanRDoesMC/sdks/archive/main.zip
 TMP=$(mktemp -d)
-unzip master.zip -d $TMP
-mv $TMP/sdks-master/*.sdk $THEOS/sdks
-rm -r master.zip $TMP
+unzip main.zip -d $TMP
+mv $TMP/sdks-main/*.sdk $THEOS/sdks
+rm -r main.zip $TMP
+echo "Done! Make sure to restart your shell."
 fi
 
 if [ "$PLATFORM_NAME" = "macOS" ]; then
