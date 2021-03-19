8921c4d23a58b30b26b8fbff53354a77cf7c85d5
EthanRDoesMC <me@ethanrdoesmc.com>
2021-03-19 16:47:50 -0500

    aye the nstask guy is here

--- LogViewController.m
+++ LogViewController.m
@@ -22,20 +22,25 @@
     NSLog(@"Hello, World!");
     NSMutableArray *arguments = [[NSMutableArray alloc] init];
     //[arguments addObject:@"/Users/ethanrdoesmc/Downloads/mautrix-imessage-amd64/mautrix-imessage"];
-    [arguments addObject:@"-c/User/Documents/mautrix-imessage-armv7/config.yaml"]; // Any arguments you want here...
-    [arguments addObject:@"-r/User/Documents/mautrix-imessage-armv7/registration.yaml"]; // Any arguments you want here...
+    //[arguments addObject:@"-c/var/mobile/Documents/mautrix-imessage-armv7/config.yaml"]; // Any arguments you want here...
+    [arguments addObject:@"-r/var/mobile/Documents/mautrix-imessage-armv7/registration.yaml"]; // Any arguments you want here...
     
     _task = [[NSTask alloc] init];
     [_task waitUntilExit];
-    _task.launchPath = @"/User/Documents/mautrix-imessage-armv7/mautrix-imessage";
+    _task.launchPath = @"/var/mobile/Documents/mautrix-imessage-armv7/mautrix-imessage";
     _task.arguments  = arguments;
-    _task.currentDirectoryPath = @"/User/Documents/mautrix-imessage-armv7/";
+    _task.currentDirectoryPath = @"/var/mobile/Documents/mautrix-imessage-armv7/";
     NSLog(@"tell me about %@", _task);
     NSMutableDictionary *defaultEnv = [[NSMutableDictionary alloc] initWithDictionary:[[NSProcessInfo processInfo] environment]];
     [defaultEnv setObject:@"YES" forKey:@"NSUnbufferedIO"];
     //            [defaultEnv setObject:@"/Users/ethanrdoesmc/" forKey:@"HOME"];
     
     _task.environment = defaultEnv;
+    
+    NSPipe *writePipe = [NSPipe pipe];
+    NSFileHandle *writeHandle = [writePipe fileHandleForWriting];
+    
+    [task setStandardInput: writePipe];
     
     _task.standardOutput = [NSPipe pipe];
     [[_task.standardOutput fileHandleForReading] setReadabilityHandler:^(NSFileHandle *file) {
@@ -46,6 +51,18 @@
         
         dispatch_async(dispatch_get_main_queue(), ^{
             self->_logField.text = [self->_logField.text stringByAppendingString: string];
+            [self->_logField scrollRangeToVisible: NSMakeRange(self->_logField.text.length, 0)];
+        });
+    }];
+    _task.standardError = [NSPipe pipe];
+    [[_task.standardError fileHandleForReading] setReadabilityHandler:^(NSFileHandle *errfile) {
+        NSData *errdata = [errfile availableData]; // this will read to EOF, so call only once
+        NSString *errstring = [[NSString alloc] initWithData:errdata encoding:NSUTF8StringEncoding];
+        NSLog(@"Task output! %@", errstring);
+        
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self->_logField.text = [self->_logField.text stringByAppendingString: errstring];
             [self->_logField scrollRangeToVisible: NSMakeRange(self->_logField.text.length, 0)];
         });
     }];
