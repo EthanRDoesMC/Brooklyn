daaade9164b47de546ad7535545de9cb501a4a4d
Ethan Chaffin <me@ethanrdoesmc.com>
2021-08-23 08:58:24 -0500

    oops

--- build/brooklyn.build/Debug-iphoneos/xchighlight.build/Objects-normal/arm64/SendMessageController.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/ethanrdoesmc/Developer/Beeper/Brooklyn-VSC/brooklyn/SendMessageController.m \
-  /Users/ethanrdoesmc/Developer/Beeper/Brooklyn-VSC/brooklyn/build/brooklyn.build/Debug-iphoneos/xchighlight.build/module.modulemap \
-  /Users/ethanrdoesmc/Developer/Beeper/Brooklyn-VSC/brooklyn/SendMessageController.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/dyld.modulemap \
-  /Users/ethanrdoesmc/Developer/Beeper/Brooklyn-VSC/brooklyn/ChatKit.h \
-  /Users/ethanrdoesmc/Developer/Beeper/Brooklyn-VSC/brooklyn/IMCore.h
--- build/brooklyn.build/Debug-iphoneos/xchighlight.build/Objects-normal/arm64/xchighlight_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/ethanrdoesmc/Developer/Beeper/Brooklyn-VSC/brooklyn/build/brooklyn.build/Debug-iphoneos/xchighlight.build/DerivedSources/xchighlight_vers.c
