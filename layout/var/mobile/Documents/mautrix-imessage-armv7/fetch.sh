9cfbb5700548e6caf226e623485a4a55df1a24f9
Carl Ambroselli <git@carl-ambroselli.de>
2021-12-04 14:45:24 +0100

    Fix build configuration and README

--- layout/var/mobile/Documents/mautrix-imessage-armv7/fetch.sh
+++ layout/var/mobile/Documents/mautrix-imessage-armv7/fetch.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cd $(dirname $0)
-curl -L https://mau.dev/tulir/mautrix-imessage/-/jobs/artifacts/master/download?job=build%20armv7 -o mautrix-imessage-armv7.zip
+curl -L https://mau.dev/mautrix/imessage/-/jobs/artifacts/master/download?job=build+ios+armv7 -o mautrix-imessage-ios-armv7.zip
 rm -f mautrix-imessage libolm.3.dylib example-config.yaml
-unzip mautrix-imessage-armv7.zip
-rm -f mautrix-imessage-armv7.zip
+unzip mautrix-imessage-ios-armv7.zip
+rm -f mautrix-imessage-ios-armv7.zip
+mv mautrix-imessage-ios-armv7/* .
+rmdir mautrix-imessage-ios-armv7
